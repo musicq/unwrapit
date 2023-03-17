344b909b121a9822f25d8c4959dec73e3de86e50
musicq <liang.kui@hotmail.com>
2023-03-17 23:26:56 +0800

    docs: update readme

--- README.md
+++ README.md
@@ -10,16 +10,75 @@ npm i unwrapit
 
 ## Usage
 
+### Synchronous function
+
+If you have a synchronous function, you can simply wrap your function and call
+it later on.
+
 ```ts
 import { wrap } from 'unwrapit'
 
-const asyncRet = wrap(new Promise<number>((r, j) => r(1))).unwrap() // 1
-const syncRet = wrap((): string => JSON.parse('123'))().unwrap() // 123
+const tryParseJson = wrap(() => JSON.parse(`{"package": "unwrapit!"}`))
+// unwrap to get the value without checking if it could be failed.
+const json = tryParseJson().unwrap()
 ```
+
+### Asynchronous function
+
+You can wrap a promise value as well.
+
+```ts
+import { wrap } from 'unwrapit'
+
+const promise = new Promise<string>((resolve, reject) => {
+  if (Math.random() < 0.5) return resolve('Yay')
+  return reject(new Error('Random number is greater than 0.5.'))
+})
+
+// ret type is string
+const ret = wrap(promise).unwrap()
+```
+
+### Check before use
+
+Normally, you should check if your value if succeed or not, then handle your
+errors. In this case, you can do this
+
+```ts
+import { wrap } from 'unwrapit'
+
+const promise = new Promise<string>((resolve, reject) => {
+  if (Math.random() < 0.5) return resolve('Yay')
+  return reject(new Error('Random number is greater than 0.5.'))
+})
+
+const ret = wrap(promise)
+
+if (!ret.ok) {
+  console.error(ret.error)
+  throw ret.error
+}
+
+console.log(ret.value)
+```
+
+### Use Result manually
+
+Normally, you can just simply `wrap` your function, it has `wrap`ped with Result
+automatically. But you still can use Result manually.
 
 ```ts
 import { ok, err } from 'unwrapit'
+import type { Result } from 'unwrapit'
 
-const promise = new Promise((r, j) => j('err')).then(ok).then(err)
-promise.unwrap() // throw error
+const pass: Result<number, never> = ok(1)
+const fail: Result<never, string> = err('error')
+
+function fn(v: boolean): Result<number, string> {
+  if (v) return ok(1)
+  return err('error')
+}
+
+const ret = fn(Math.random() < 0.5)
+console.log(ret.unwrap())
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-export type Ok<T> = {
+type Ok<T> = {
   ok: true
   value: T
   unwrap: () => T
 }
 
-export type Err<E = unknown> = {
+type Err<E = unknown> = {
   ok: false
   error: E
   unwrap: () => never
@@ -12,10 +12,50 @@ export type Err<E = unknown> = {
 
 export type Result<T, E = unknown> = Ok<T> | Err<E>
 
+/**
+ * Wrap an synchronous function. This is useful when you are trying to parse an
+ * JSON through `JSON.parse`.
+ *
+ * # Example
+ *
+ * ```ts
+ * import {wrap} from 'unwrapit'
+ *
+ * const tryParseJson = wrap(() => JSON.parse(`{"package": "unwrapit!"}`))
+ * // unwrap to get the value without checking if it could be failed.
+ * const json = tryParseJson().unwrap()
+ * ```
+ */
 export function wrap<TArgs extends any[], TReturn>(
   func: (...args: TArgs) => TReturn
 ): (...args: TArgs) => Result<TReturn>
+
+/**
+ * Wrap an promise value. This allows you could handle error gracefully.
+ *
+ * # Example
+ * ```ts
+ * import {wrap} from 'unwrapit'
+ *
+ * const promise = new Promise<string>((resolve, reject) => {
+ *   if (Math.random() < 0.5) return resolve('Yay')
+ *   return reject(new Error('Random number is greater than 0.5.'))
+ * })
+ *
+ * // ret type is string
+ * const ret = wrap(promise).unwrap()
+ * ```
+ */
 export function wrap<T extends Promise<any>>(promise: T): Result<Awaited<T>>
+
+/**
+ * Wrap either a function or a promise value
+ *
+ * # Example
+ * ```ts
+ * import {wrap} from 'unwrapit
+ * ```
+ */
 export function wrap(input: any) {
   if (typeof input === 'function') {
     return (...args: any[]) => {
@@ -30,23 +70,22 @@ export function wrap(input: any) {
   return input.then(ok).catch(err)
 }
 
-export function err<T = unknown>(e: T): Err<T> {
-  return {
-    ok: false,
-    error: e,
-    unwrap() {
-      console.error(e)
-      throw e
-    },
-  }
-}
-
-export function ok<T>(v: T): Ok<T> {
+export function ok<T>(v: T): Result<T, never> {
   return {
     ok: true,
     value: v,
     unwrap() {
       return v
+    },
+  }
+}
+
+export function err<T = unknown>(e: T): Result<never, T> {
+  return {
+    ok: false,
+    error: e,
+    unwrap() {
+      throw e
     },
   }
 }
