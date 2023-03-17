3635a8e22cd7e4c491cd15bfe0337dcabe16ae6a
Liang Kui <kui.liang@shopee.com>
2023-03-17 18:38:57 +0800

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+export type Ok<T> = {
+  ok: true
+  value: T
+  unwrap: () => T
+}
+
+export type Err<E = unknown> = {
+  ok: false
+  error: E
+  unwrap: () => never
+}
+
+export type Result<T, E = unknown> = Ok<T> | Err<E>
+
+export function wrap<TArgs extends any[], TReturn>(
+  func: (...args: TArgs) => TReturn
+): (...args: TArgs) => Result<TReturn>
+export function wrap<T extends Promise<any>>(promise: T): Result<Awaited<T>>
+export function wrap(input: any) {
+  if (typeof input === 'function') {
+    return (...args: any[]) => {
+      try {
+        return ok(input(...args))
+      } catch (e) {
+        return err(e)
+      }
+    }
+  }
+
+  return input.then(ok).catch(err)
+}
+
+export function err<T = unknown>(e: T): Err<T> {
+  return {
+    ok: false,
+    error: e,
+    unwrap() {
+      console.error(e)
+      throw e
+    },
+  }
+}
+
+export function ok<T>(v: T): Ok<T> {
+  return {
+    ok: true,
+    value: v,
+    unwrap() {
+      return v
+    },
+  }
+}
+
+// panic if there is error in unwrap
+// const async_ret = wrap(new Promise<number>((r, j) => r(1))).unwrap()
+// const sync_ret = wrap((): string => JSON.parse('123'))().unwrap()
+
+// let a = ok(1)
+// let e = err<number>(1)
