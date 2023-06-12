f442383a9b0439682b0c356276c31da05aa133fd
musicq <kui.liang.sg@gmail.com>
2023-06-12 09:20:57 +0800

    test: add tests for toWrap operator

--- tests/result.test.ts
+++ tests/result.test.ts
@@ -1,7 +1,8 @@
 import {panic} from 'panicit'
+import {count, from, last, map, take} from 'rxjs'
 import {afterEach, describe, expect, test, vi} from 'vitest'
-import {setPanic} from '../src'
-import {Panic, err, ok, wrap} from '../src/result'
+import {Panic, err, ok, setPanic, wrap} from '../src/result'
+import {toWrap} from '../src/toWrap'
 
 vi.mock('panicit', async () => {
   const mod = await vi.importActual('panicit')
@@ -215,6 +216,42 @@ describe('result', () => {
 
       expect(errRet.ok).toBe(false)
       expect(errRet.error as string).toBe('async error')
+    })
+  })
+
+  describe('toWrap function', () => {
+    test('convert value to Result', () => {
+      const sub$ = from([1, 2, 3]).pipe(
+        map((x) => {
+          if (x % 2 === 0) throw new Error(`num ${x} is even.`)
+          return x
+        }),
+        toWrap()
+      )
+
+      sub$.pipe(take(1)).subscribe((x) => {
+        expect(x.unwrap()).toBe(1)
+      })
+
+      sub$.pipe(count()).subscribe((x) => expect(x).toBe(2))
+
+      sub$.pipe(last()).subscribe((x) => {
+        if (x.ok) {
+          throw new Error('This line should never be reached.')
+        }
+
+        expect(x.error).toBe('num 2 is even.')
+      })
+
+      const complete$ = from([1, 2, 3]).pipe(toWrap())
+      complete$.pipe(count()).subscribe((x) => expect(x).toBe(3))
+      complete$.subscribe({
+        next: (x) => expect(x.ok).toBe(true),
+        // never reach
+        error: () => expect(false).toBe(true),
+        // will reach
+        complete: () => expect(true).toBe(true),
+      })
     })
   })
 
