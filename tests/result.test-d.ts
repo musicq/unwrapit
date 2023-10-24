38f04a5b981545c401ff7b320129b61f371e22b1
Liang Kui <kui.liang.sg@gmail.com>
2023-10-24 18:36:06 +0800

    test: complete tests

--- tests/result.test-d.ts
+++ tests/result.test-d.ts
@@ -1,49 +1,97 @@
 import {describe, expectTypeOf, test} from 'vitest'
-import {Err, Ok, Result, err, ok, wrap} from '../src/result'
+import {Err, Ok, Result, WrapOption, err, ok, wrap} from '../src/result'
+import {TF} from '../src/types'
 
 describe('Result types test', () => {
   test('ok type', () => {
-    expectTypeOf(ok).returns.toMatchTypeOf<Result<unknown, never>>()
-    expectTypeOf(ok(1)).toMatchTypeOf<Result<number, never>>()
-    expectTypeOf(ok('string')).toMatchTypeOf<Result<string, never>>()
-    expectTypeOf((ok(1) as Ok<number>).unwrap).toMatchTypeOf<() => number>()
-    expectTypeOf((ok(1) as Ok<number>).unwrapOr).toMatchTypeOf<
-      (v: number) => number
+    expectTypeOf(ok).returns.toEqualTypeOf<Result<unknown, never>>()
+    expectTypeOf(ok(1)).toEqualTypeOf<Result<number, never>>()
+    expectTypeOf(ok('string')).toEqualTypeOf<Result<string, never>>()
+    expectTypeOf(ok(1).unwrap).toMatchTypeOf<(opts?: WrapOption) => number>()
+    expectTypeOf(ok(1).unwrapOr).toEqualTypeOf<(v: number) => number>()
+    expectTypeOf(ok(1).unwrapOrElse(e => 'string')).toEqualTypeOf<
+      string | number
     >()
-    expectTypeOf((ok(1) as Ok<number>).expect).toMatchTypeOf<
-      (arg: string) => number
+    expectTypeOf(
+      (ok(1) as Ok<number>).unwrapOrElse(e => '1')
+    ).toEqualTypeOf<number>()
+    expectTypeOf(ok(1).expect).toMatchTypeOf<
+      (arg: string, opts?: WrapOption) => number
     >()
   })
 
   test('err type', () => {
-    expectTypeOf(err).returns.toMatchTypeOf<Result<any, unknown>>()
-    expectTypeOf(err(1)).toMatchTypeOf<Result<any, number>>()
-    expectTypeOf(err('string')).toMatchTypeOf<Result<any, string>>()
-    expectTypeOf(err(new Error())).toMatchTypeOf<Result<any, Error>>()
-    expectTypeOf((err(1) as Err<unknown, number>).unwrap).toMatchTypeOf<
-      () => never
+    expectTypeOf(err).returns.toEqualTypeOf<Result<unknown, unknown>>()
+    expectTypeOf(err(1)).toEqualTypeOf<Result<unknown, number>>()
+    expectTypeOf(err('string')).toEqualTypeOf<Result<unknown, string>>()
+    expectTypeOf(err(new Error())).toEqualTypeOf<Result<unknown, Error>>()
+    expectTypeOf((err(1) as Err<number, string>).unwrap).toMatchTypeOf<
+      (opts?: WrapOption) => never
     >()
-    expectTypeOf((err(1) as Err<unknown, number>).unwrapOr).toMatchTypeOf<
-      (v: number) => number
+    expectTypeOf((err(1) as Err<number, string>).unwrapOr).toEqualTypeOf<
+      (v: string) => string
     >()
-    expectTypeOf((err(1) as Err<unknown, number>).expect).toMatchTypeOf<
-      (errorMessage: string) => never
+    expectTypeOf((err(1) as Err<number, string>).unwrapOrElse).toEqualTypeOf<
+      <U>(mapFn: (e: number) => U) => U
+    >()
+    expectTypeOf((err(1) as Err<number, string>).expect).toMatchTypeOf<
+      (errorMessage: string, opts?: WrapOption) => never
+    >()
+    expectTypeOf(err(1).mapErr).toMatchTypeOf<
+      <U>(errMapFn: (e: number) => U) => Result<unknown, U>
     >()
   })
 
-  test('wrap type', () => {
-    // sync
-    expectTypeOf(wrap(() => {})).toMatchTypeOf<() => Result<void, unknown>>()
-    expectTypeOf(wrap((a: boolean) => 1)).toMatchTypeOf<
-      (a: boolean) => Result<number, unknown>
-    >()
+  describe('wrap type', () => {
+    test('async functions', () => {
+      async function foo(a: number, b: boolean): Promise<string> {
+        return ''
+      }
 
-    // async
-    expectTypeOf(wrap(Promise.resolve('ok'))).toMatchTypeOf<
-      Promise<Result<string, unknown>>
-    >()
-    expectTypeOf(wrap(Promise.reject('err'))).toMatchTypeOf<
-      Promise<Result<never, unknown>>
-    >()
+      expectTypeOf(wrap(foo)).toEqualTypeOf<
+        (a: number, b: boolean) => Promise<Result<string, unknown>>
+      >()
+      expectTypeOf(wrap<Error, TF<typeof foo>>(foo)).toEqualTypeOf<
+        (a: number, b: boolean) => Promise<Result<string, Error>>
+      >()
+    })
+
+    test('sync functions', () => {
+      function foo(a: number, b: boolean): string {
+        return ''
+      }
+
+      expectTypeOf(wrap(foo)).toEqualTypeOf<
+        (a: number, b: boolean) => Result<string, unknown>
+      >()
+      expectTypeOf(wrap<Error, TF<typeof foo>>(foo)).toEqualTypeOf<
+        (a: number, b: boolean) => Result<string, Error>
+      >()
+    })
+
+    test('promise values', () => {
+      expectTypeOf(wrap(Promise.resolve(1))).toEqualTypeOf<
+        Promise<Result<number, unknown>>
+      >()
+      expectTypeOf(wrap<Error, number>(Promise.resolve(1))).toEqualTypeOf<
+        Promise<Result<number, Error>>
+      >()
+      expectTypeOf(wrap(Promise.reject(1))).toEqualTypeOf<
+        Promise<Result<never, unknown>>
+      >()
+    })
+
+    test('arbitrary values', () => {
+      expectTypeOf(wrap(1)).toEqualTypeOf<Result<number, never>>()
+      expectTypeOf(wrap('string')).toEqualTypeOf<Result<string, never>>()
+      expectTypeOf(wrap([1, 2, 3])).toEqualTypeOf<Result<number[], never>>()
+      expectTypeOf(wrap({a: 1, b: true})).toEqualTypeOf<
+        Result<{a: number; b: boolean}, never>
+      >()
+      expectTypeOf(wrap(new Error())).toEqualTypeOf<Result<Error, never>>()
+
+      // @ts-expect-error
+      expectTypeOf(wrap<string>(1)).toEqualTypeOf<Result<string, never>>()
+    })
   })
 })
